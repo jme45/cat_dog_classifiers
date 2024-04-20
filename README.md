55e3bcffc99e03a1b06a88cfaf1a26b887de2d01
Jonathan Edge <j.edge@zoho.com>
2024-04-20 08:48:15 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,7 @@
+# cat_dog_classifiers
+
+This is not expected to be of sufficient general interest that other people might want to install it, but for my personal use it is useful to have it on pypi.
+
+## Installation
+pip install git+https:// ...
+
---
+++ src/cat_dog_classifiers/classifiers.py
@@ -0,0 +1,43 @@
+"""
+Defines the classifiers for the dog/cat classification
+"""
+# TODO: turn into a project. create: pyproject.toml, README.md, LICENSE.md
+# TODO: upload to github, so can install via pip install git+https://...
+# TODO: create git repo. compare to aircraft_classifiers
+from pathlib import Path
+from typing import Optional
+
+from torchvision.transforms import v2 as transf_v2
+from transfer_learning_vision_classifiers import classifiers as tlvs_classifiers
+
+
+class CatDogClassifier(tlvs_classifiers.TransferLearningVisionClassifier):
+    def __init__(
+        self,
+        model_type: str,
+        class_names=["cat", "dog"],
+        load_classifier_pretrained_weights: bool = False,
+        classifier_pretrained_weights_file: Optional[str | Path] = None,
+        data_augmentation_transforms: transf_v2.Transform = transf_v2.Identity(),
+    ):
+        """
+        Initialise a binary classifier for cats and dogs.
+
+        :param model_type: "vit_b_16", "vit_l_16", "effnet_b2", "effnet_b7", "trivial"
+        :param class_names: Names for the two classes. Sensibly should be 'cat' and 'dog'
+        :param load_classifier_pretrained_weights: whether to load classifier
+        :param classifier_pretrained_weights_file: file for classifier data
+        """
+        self.class_names = class_names
+        # Define train and prediction transform. Unlike for aircraft classification,
+        # we don't need a transform to crop the authorship information
+        self.train_transform = transf_v2.Compose(
+            [
+                tlvs_classifiers.TO_TENSOR_TRANSFORMS,
+                data_augmentation_transforms,
+                self.transforms,
+            ]
+        )
+        self.predict_transform = transf_v2.Compose(
+            [tlvs_classifiers.TO_TENSOR_TRANSFORMS, self.transforms]
+        )
